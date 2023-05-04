38cfb03261b33ad22d19e0076cd20868f0c8b524
Benjamin Correa <iambenjamin.developer@gmail.com>
2023-05-04 11:00:35 -0300

    quitar WeatherForecast

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new string[] { "value1", "value2" };
+
+            return Ok(result);
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = $"value: {id}";
+
+            return Ok(result);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Created(nameof(GetById), value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok(value);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(id);
+        }
+    }
+}
